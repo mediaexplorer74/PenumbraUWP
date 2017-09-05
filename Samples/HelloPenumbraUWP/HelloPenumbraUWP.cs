978b9335633cb0cbbf7028f5c7dfbbcebea60f9f
Nicholas Chai (Reynolds) <nick@nickchai.com>
2017-09-05 12:06:24 +0800

    removed all other sample apps Got the HelloPenumbraUWP sample app running - added the original hello penumbra code - added the required content files to the content pipeline

--- Samples/HelloPenumbraUWP/HelloPenumbraUWP.cs
+++ Samples/HelloPenumbraUWP/HelloPenumbraUWP.cs
@@ -1,5 +1,8 @@
 ﻿using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using Penumbra;
+using System;
 
 namespace HelloPenumbraUWP
 {
@@ -11,10 +14,30 @@ namespace HelloPenumbraUWP
         GraphicsDeviceManager graphics;
         SpriteBatch spriteBatch;
 
+        // Store reference to lighting system.
+        PenumbraComponent penumbra;
+
+        // Create sample light source and shadow hull.
+        Light light = new PointLight
+        {
+            Scale = new Vector2(1000f), // Range of the light source (how far the light will travel)
+            ShadowType = ShadowType.Solid // Will not lit hulls themselves
+        };
+        Hull hull = new Hull(new Vector2(1.0f), new Vector2(-1.0f, 1.0f), new Vector2(-1.0f), new Vector2(1.0f, -1.0f))
+        {
+            Position = new Vector2(400f, 240f),
+            Scale = new Vector2(50f)
+        };
+
         public HelloPenumbraUWP()
         {
             graphics = new GraphicsDeviceManager(this);
             Content.RootDirectory = "Content";
+
+            // Create the lighting system and add sample light and hull.
+            penumbra = new PenumbraComponent(this);
+            penumbra.Lights.Add(light);
+            penumbra.Hulls.Add(hull);
         }
 
         /// <summary>
@@ -26,6 +49,9 @@ namespace HelloPenumbraUWP
         protected override void Initialize()
         {
             // TODO: Add your initialization logic here
+
+            // Initialize the lighting system.
+            penumbra.Initialize();
 
             base.Initialize();
         }
@@ -60,6 +86,17 @@ namespace HelloPenumbraUWP
         {
             // TODO: Add your update logic here
 
+            if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
+                Exit();
+
+            // Animate light position and hull rotation.
+            light.Position =
+                new Vector2(400f, 240f) + // Offset origin
+                new Vector2( // Position around origin
+                    (float)Math.Cos(gameTime.TotalGameTime.TotalSeconds),
+                    (float)Math.Sin(gameTime.TotalGameTime.TotalSeconds)) * 240f;
+            hull.Rotation = MathHelper.WrapAngle(-(float)gameTime.TotalGameTime.TotalSeconds);
+
             base.Update(gameTime);
         }
 
@@ -69,9 +106,17 @@ namespace HelloPenumbraUWP
         /// <param name="gameTime">Provides a snapshot of timing values.</param>
         protected override void Draw(GameTime gameTime)
         {
+            // Everything between penumbra.BeginDraw and penumbra.Draw will be
+            // lit by the lighting system.
+            penumbra.BeginDraw();
+
             GraphicsDevice.Clear(Color.CornflowerBlue);
 
-            // TODO: Add your drawing code here
+            // Draw items affected by lighting here ...
+
+            penumbra.Draw(gameTime);
+
+            // Draw items NOT affected by lighting here ... (UI, for example)
 
             base.Draw(gameTime);
         }
